3c415a420a888f6a0da3c1da0be456eab7390f25
TOMOHIRO <hurandall@yahoo.co.jp>
2021-02-15 03:26:54 +0900

    ローダーまで

---
+++ app/assets/javascripts/music_choices.js
@@ -0,0 +1,12 @@
+$(function() {
+  let h = $(window).height();
+  $('#butohes-log').height(h).css('display','block');
+  $('#butohes-log-image').css('display','block');
+});
+
+$(window).on('load', function () { //全ての読み込みが完了したら実行
+  $('#butohes-log-image').delay(1500).fadeOut(800);
+  $('#butohes-log').delay(2500).fadeOut(300);
+  $('#global-container').css('display','block');
+  $('#global-container').hide().fadeIn(8000);
+  });
