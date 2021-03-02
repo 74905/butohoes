c8f2bd45b2002ecaaca33b8d84ea593aa2bb4dfb
TOMOHIRO <hurandall@yahoo.co.jp>
2021-03-02 21:43:04 +0900

    topカードまで

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,15 @@
 class NewsController < ApplicationController
+  def new
+    @news= News.new
+  end
+  
+  def create
+    news = News.new(news_params)
+    news.save
+    redirect_to root_path
+  end
+  private
+  def news_params
+    params.require(:news).permit(:title, :body, :news_image)
+  end
 end
